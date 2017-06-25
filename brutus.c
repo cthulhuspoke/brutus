cb3d26817ec8e04ab219b41e7deb7ad7beb95c23
cthuluspoke <cthulu.spoke@gmail.com>
2017-06-25 14:52:13 -0400

    Update brutus.c

--- brutus.c
+++ brutus.c
@@ -18,7 +18,7 @@ int main (int argc, char* argv[])
     if(argc != 2)
     {
         printf("Usage: ./crack hash \n");
-        return 1;
+        return (1);
     }
     
     char* hash = argv[1];
